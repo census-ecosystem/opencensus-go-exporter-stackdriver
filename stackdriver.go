857ff689ec3b8d8ebb64827938f1ff602eee59a9
Jean de Klerk <deklerk@google.com>
2018-09-28 09:22:15 -0700

    docs: remove premium notice (#47)

--- stackdriver.go
+++ stackdriver.go
@@ -38,7 +38,6 @@
 //   1. Create a Cloud project: https://support.google.com/cloud/answer/6251787?hl=en
 //   2. Enable billing: https://support.google.com/cloud/answer/6288653#new-billing
 //   3. Enable the Stackdriver Monitoring API: https://console.cloud.google.com/apis/dashboard
-//   4. Make sure you have a Premium Stackdriver account: https://cloud.google.com/monitoring/accounts/tiers
 //
 // These steps enable the API but don't require that your app is hosted on Google Cloud Platform.
 //
