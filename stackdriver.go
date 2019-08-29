f0aaf14c4fcc393ece13d3426d125af817e1facc
songy23 <songya@google.com>
2019-08-29 16:57:09 -0700

    Remove ExportView

--- stackdriver.go
+++ stackdriver.go
@@ -341,11 +341,6 @@ func NewExporter(o Options) (*Exporter, error) {
 	}, nil
 }
 
-// ExportView exports to the Stackdriver Monitoring if view data
-// has one or more rows.
-// Deprecated: this method does not have any effect now, use ExportMetrics instead.
-func (e *Exporter) ExportView(vd *view.Data) {}
-
 // ExportMetricsProto exports OpenCensus Metrics Proto to Stackdriver Monitoring synchronously,
 // without de-duping or adding proto metrics to the bundler.
 func (e *Exporter) ExportMetricsProto(ctx context.Context, node *commonpb.Node, rsc *resourcepb.Resource, metrics []*metricspb.Metric) error {
