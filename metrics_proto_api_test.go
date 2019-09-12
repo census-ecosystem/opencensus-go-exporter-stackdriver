5e7c96610592fdcccce9a302b34cadde0895c54d
rahulpa <rahulpa@google.com>
2019-09-12 14:37:00 -0700

    add test for max timeseries per request.

--- metrics_proto_api_test.go
+++ metrics_proto_api_test.go
@@ -42,104 +42,145 @@ import (
 	//"go.opencensus.io/resource/resourcekeys"
 )
 
-func TestExportTimeSeriesWithDifferentLabels(t *testing.T) {
-	server, addr, doneFn := createFakeServer(t)
-	defer doneFn()
+type testCases struct {
+	name     string
+	inMetric []*metricspb.Metric
+	outTSR   []*monitoringpb.CreateTimeSeriesRequest
+	outMDR   []*monitoringpb.CreateMetricDescriptorRequest
+}
 
-	// Now create a gRPC connection to the agent.
-	conn, err := grpc.Dial(addr, grpc.WithInsecure())
-	if err != nil {
-		t.Fatalf("Failed to make a gRPC connection to the agent: %v", err)
-	}
-	defer conn.Close()
+var (
+	// project
+	projectID = "metrics_proto_test"
 
-	// Finally create the OpenCensus stats exporter
-	exporterOptions := sd.Options{
-		ProjectID:               "metrics_proto_test",
-		MonitoringClientOptions: []option.ClientOption{option.WithGRPCConn(conn)},
-
+	// default exporter options
+	defaultOpts = sd.Options{
+		ProjectID: projectID,
 		// Set empty labels to avoid the opencensus-task
 		DefaultMonitoringLabels: &sd.Labels{},
 	}
-	se, err := sd.NewExporter(exporterOptions)
-	if err != nil {
-		t.Fatalf("Failed to create the statsExporter: %v", err)
+
+	// resources
+	outGlobalResource = &monitoredrespb.MonitoredResource{
+		Type: "global",
 	}
 
 	// timestamps
-	startTimestamp := &timestamp.Timestamp{
+	startTimestamp = &timestamp.Timestamp{
 		Seconds: 1543160298,
 		Nanos:   100000090,
 	}
-	endTimestamp := &timestamp.Timestamp{
+	endTimestamp = &timestamp.Timestamp{
 		Seconds: 1543160298,
 		Nanos:   100000997,
 	}
-	outInterval := &monitoringpb.TimeInterval{
+	outInterval = &monitoringpb.TimeInterval{
 		StartTime: startTimestamp,
 		EndTime:   endTimestamp,
 	}
 
 	// label keys and values
-	inEmptyKey := &metricspb.LabelKey{Key: "empty_key"}
-	inOperTypeKey := &metricspb.LabelKey{Key: "operation_type"}
-	inNoValue := &metricspb.LabelValue{Value: "", HasValue: false}
-	inEmptyValue := &metricspb.LabelValue{Value: "", HasValue: true}
-	inOperTypeValue1 := &metricspb.LabelValue{Value: "test_1", HasValue: true}
-	inOperTypeValue2 := &metricspb.LabelValue{Value: "test_2", HasValue: true}
+	inEmptyKey       = &metricspb.LabelKey{Key: "empty_key"}
+	inOperTypeKey    = &metricspb.LabelKey{Key: "operation_type"}
+	inNoValue        = &metricspb.LabelValue{Value: "", HasValue: false}
+	inEmptyValue     = &metricspb.LabelValue{Value: "", HasValue: true}
+	inOperTypeValue1 = &metricspb.LabelValue{Value: "test_1", HasValue: true}
+	inOperTypeValue2 = &metricspb.LabelValue{Value: "test_2", HasValue: true}
+
+	// Metric Descriptor
+	inMetricNameCalls    = "ocagent.io/calls"
+	outCreateMDNameCalls = "projects/" + projectID + "/metricDescriptors/custom.googleapis.com/opencensus/" + inMetricNameCalls
+	outMetricTypeCalls   = "custom.googleapis.com/opencensus/" + inMetricNameCalls
+	outDisplayNameCalls  = "OpenCensus/" + inMetricNameCalls
+
+	inMetricDescCalls  = "Number of calls"
+	outMetricDescCalls = "Number of calls"
+
+	metricUnitCalls    = "1"
+	outMetricUnitCalls = "1"
+
+	inMDCall = createMetricPbDescriptor(inMetricNameCalls,
+		inMetricDescCalls,
+		metricUnitCalls,
+		metricspb.MetricDescriptor_CUMULATIVE_INT64,
+		inEmptyKey,
+		inOperTypeKey)
+	outMDCall = createGoogleMetricPbDescriptor(
+		outCreateMDNameCalls,
+		outMetricTypeCalls,
+		outMetricDescCalls,
+		outDisplayNameCalls,
+		outMetricUnitCalls,
+		googlemetricpb.MetricDescriptor_CUMULATIVE,
+		googlemetricpb.MetricDescriptor_INT64,
+		"empty_key",
+		"operation_type")
+
+	inMetricNameLatency    = "ocagent.io/latency"
+	outCreateMDNameLatency = "projects/" + projectID + "/metricDescriptors/custom.googleapis.com/opencensus/" + inMetricNameLatency
+	outMetricTypeLatency   = "custom.googleapis.com/opencensus/" + inMetricNameLatency
+	outDisplayNameLatency  = "OpenCensus/" + inMetricNameLatency
+
+	inMetricDescLatency  = "Sum of latency"
+	outMetricDescLatency = "Sum of latency"
+
+	metricUnitLatency    = "ms"
+	outMetricUnitLatency = "ms"
+
+	inMDLatency = createMetricPbDescriptor(inMetricNameLatency,
+		inMetricDescLatency,
+		metricUnitLatency,
+		metricspb.MetricDescriptor_CUMULATIVE_INT64,
+		inEmptyKey,
+		inOperTypeKey)
+	outMDLatency = createGoogleMetricPbDescriptor(
+		outCreateMDNameLatency,
+		outMetricTypeLatency,
+		outMetricDescLatency,
+		outDisplayNameLatency,
+		outMetricUnitLatency,
+		googlemetricpb.MetricDescriptor_CUMULATIVE,
+		googlemetricpb.MetricDescriptor_INT64,
+		"empty_key",
+		"operation_type")
 
 	// points
-	inPointsInt64_1 := []*metricspb.Point{
+	inPointsInt64_1 = []*metricspb.Point{
 		{
 			Timestamp: endTimestamp,
 			Value:     &metricspb.Point_Int64Value{Int64Value: int64(1)},
 		},
 	}
-	outValueInt64_1 := &monitoringpb.TypedValue{
+	outValueInt64_1 = &monitoringpb.TypedValue{
 		Value: &monitoringpb.TypedValue_Int64Value{
 			Int64Value: 1,
 		},
 	}
-	outPointsInt64_1 := []*monitoringpb.Point{
+	outPointsInt64_1 = []*monitoringpb.Point{
 		{
 			Interval: outInterval,
 			Value:    outValueInt64_1,
 		},
 	}
+)
 
-	// resources
-	outGlobalResource := &monitoredrespb.MonitoredResource{
-		Type: "global",
-	}
+func TestExportLabels(t *testing.T) {
+	server, addr, doneFn := createFakeServer(t)
+	defer doneFn()
 
-	metricNameCalls := "ocagent.io/calls"
-	outCreateMDNameCalls := "projects/metrics_proto_test/metricDescriptors/custom.googleapis.com/opencensus/" + metricNameCalls
-	outMetricTypeCalls := "custom.googleapis.com/opencensus/" + metricNameCalls
-	outDisplayName := "OpenCensus/" + metricNameCalls
+	// Now create a gRPC connection to the agent.
+	conn := createConn(t, addr)
+	defer conn.Close()
 
-	metricDescCalls := "Number of calls"
-	outCreateMDDescCalls := "Number of calls"
+	// Finally create the OpenCensus stats exporter
+	se := createExporter(t, conn, defaultOpts)
 
-	metricUnit := "1"
-	outMetricUnit := "1"
-
-	testCases := []struct {
-		name     string
-		inMetric []*metricspb.Metric
-		outTSR   []*monitoringpb.CreateTimeSeriesRequest
-		outMDR   []*monitoringpb.CreateMetricDescriptorRequest
-	}{
+	tcs := []*testCases{
 		{
 			name: "Label Test: [empty,v1], [empty,v2], [noValue,v1], [empty,noValue]",
 			inMetric: []*metricspb.Metric{
 				{
-					MetricDescriptor: &metricspb.MetricDescriptor{
-						Name:        metricNameCalls,
-						Description: metricDescCalls,
-						LabelKeys:   []*metricspb.LabelKey{inEmptyKey, inOperTypeKey},
-						Unit:        metricUnit,
-						Type:        metricspb.MetricDescriptor_CUMULATIVE_INT64,
-					},
+					MetricDescriptor: inMDCall,
 					Timeseries: []*metricspb.TimeSeries{
 						{
 							StartTimestamp: startTimestamp,
@@ -223,55 +264,246 @@ func TestExportTimeSeriesWithDifferentLabels(t *testing.T) {
 			},
 			outMDR: []*monitoringpb.CreateMetricDescriptorRequest{
 				{
-					Name: "projects/metrics_proto_test",
-					MetricDescriptor: &googlemetricpb.MetricDescriptor{
-						Name: outCreateMDNameCalls,
-						Type: outMetricTypeCalls,
-						Labels: []*labelpb.LabelDescriptor{
-							{
-								Key:       "empty_key",
-								ValueType: labelpb.LabelDescriptor_STRING,
-							},
-							{
-								Key:       "operation_type",
-								ValueType: labelpb.LabelDescriptor_STRING,
-							},
-						},
-						MetricKind:  googlemetricpb.MetricDescriptor_CUMULATIVE,
-						ValueType:   googlemetricpb.MetricDescriptor_INT64,
-						Unit:        outMetricUnit,
-						Description: outCreateMDDescCalls,
-						DisplayName: outDisplayName,
-					},
+					Name:             "projects/metrics_proto_test",
+					MetricDescriptor: outMDCall,
 				},
 			},
 		},
 	}
-	for i, tt := range testCases {
-		dropped, err := se.PushMetricsProto(context.Background(), nil, nil, tt.inMetric)
-		if dropped != 0 || err != nil {
-			t.Fatalf("#%d, Name: %s, Error pushing metrics, dropped:%d err:%v", i, tt.name, dropped, err)
+	for _, tc := range tcs {
+		executeTestCase(t, tc, se, server)
+	}
+}
+
+func TestExportMaxTSPerRequest(t *testing.T) {
+	server, addr, doneFn := createFakeServer(t)
+	defer doneFn()
+
+	// Now create a gRPC connection to the agent.
+	conn := createConn(t, addr)
+	defer conn.Close()
+
+	// Finally create the OpenCensus stats exporter
+	se := createExporter(t, conn, defaultOpts)
+
+	tcs := []*testCases{
+		{
+			name: "Metric with 250 TimeSeries, splits into 2 requests",
+			inMetric: []*metricspb.Metric{
+				{
+					MetricDescriptor: inMDCall,
+					Timeseries:       []*metricspb.TimeSeries{},
+				},
+			},
+			outTSR: []*monitoringpb.CreateTimeSeriesRequest{
+				{
+					Name:       "projects/metrics_proto_test",
+					TimeSeries: []*monitoringpb.TimeSeries{},
+				},
+				{
+					Name:       "projects/metrics_proto_test",
+					TimeSeries: []*monitoringpb.TimeSeries{},
+				},
+			},
+			outMDR: []*monitoringpb.CreateMetricDescriptorRequest{
+				{
+					Name:             "projects/metrics_proto_test",
+					MetricDescriptor: outMDCall,
+				},
+			},
+		},
+	}
+	for i := 0; i < 250; i++ {
+		v := fmt.Sprintf("value_%d", i)
+		lv := &metricspb.LabelValue{Value: v, HasValue: true}
+		ts := &metricspb.TimeSeries{
+			StartTimestamp: startTimestamp,
+			LabelValues:    []*metricspb.LabelValue{inEmptyValue, lv},
+			Points:         inPointsInt64_1,
 		}
+		tcs[0].inMetric[0].Timeseries = append(tcs[0].inMetric[0].Timeseries, ts)
 
-		var gotTimeSeries []*monitoringpb.CreateTimeSeriesRequest
-		server.forEachStackdriverTimeSeries(func(sdt *monitoringpb.CreateTimeSeriesRequest) {
-			gotTimeSeries = append(gotTimeSeries, sdt)
-		})
-
-		if diff, idx := requireTimeSeriesRequestEqual(t, gotTimeSeries, tt.outTSR); diff != "" {
-			t.Errorf("#%d, Name[%s], TimeSeries[%d], Error: %s", i, tt.name, idx, diff)
+		j := i / 200
+		outTS := &monitoringpb.TimeSeries{
+			Metric: &googlemetricpb.Metric{
+				Type: outMetricTypeCalls,
+				Labels: map[string]string{
+					"empty_key":      "",
+					"operation_type": v,
+				},
+			},
+			Resource:   outGlobalResource,
+			MetricKind: googlemetricpb.MetricDescriptor_CUMULATIVE,
+			ValueType:  googlemetricpb.MetricDescriptor_INT64,
+			Points:     outPointsInt64_1,
 		}
+		tcs[0].outTSR[j].TimeSeries = append(tcs[0].outTSR[j].TimeSeries, outTS)
+	}
+	for _, tc := range tcs {
+		executeTestCase(t, tc, se, server)
+	}
+}
 
-		var gotCreateMDRequest []*monitoringpb.CreateMetricDescriptorRequest
-		server.forEachStackdriverMetricDescriptor(func(sdm *monitoringpb.CreateMetricDescriptorRequest) {
-			gotCreateMDRequest = append(gotCreateMDRequest, sdm)
-		})
+func TestExportMaxTSPerRequestAcrossTwoMetrics(t *testing.T) {
+	server, addr, doneFn := createFakeServer(t)
+	defer doneFn()
 
-		if diff, idx := requireMetricDescriptorRequestEqual(t, gotCreateMDRequest, tt.outMDR); diff != "" {
-			t.Errorf("#%d, Name[%s], MetricDescriptor[%d], Error: %s", i, tt.name, idx, diff)
+	// Now create a gRPC connection to the agent.
+	conn := createConn(t, addr)
+	defer conn.Close()
+
+	// Finally create the OpenCensus stats exporter
+	se := createExporter(t, conn, defaultOpts)
+
+	tcs := []*testCases{
+		{
+			name: "Two Metric with 250 TimeSeries each, splits into 3 TS requests and 2 MD request",
+			inMetric: []*metricspb.Metric{
+				{
+					MetricDescriptor: inMDCall,
+					Timeseries:       []*metricspb.TimeSeries{},
+				},
+				{
+					MetricDescriptor: inMDLatency,
+					Timeseries:       []*metricspb.TimeSeries{},
+				},
+			},
+			outTSR: []*monitoringpb.CreateTimeSeriesRequest{
+				{
+					Name:       "projects/metrics_proto_test",
+					TimeSeries: []*monitoringpb.TimeSeries{},
+				},
+				{
+					Name:       "projects/metrics_proto_test",
+					TimeSeries: []*monitoringpb.TimeSeries{},
+				},
+				{
+					Name:       "projects/metrics_proto_test",
+					TimeSeries: []*monitoringpb.TimeSeries{},
+				},
+			},
+			outMDR: []*monitoringpb.CreateMetricDescriptorRequest{
+				{
+					Name:             "projects/metrics_proto_test",
+					MetricDescriptor: outMDCall,
+				},
+				{
+					Name:             "projects/metrics_proto_test",
+					MetricDescriptor: outMDLatency,
+				},
+			},
+		},
+	}
+	for i := 0; i < 500; i++ {
+		k := i / 250
+		v := fmt.Sprintf("value_%d", i)
+		lv := &metricspb.LabelValue{Value: v, HasValue: true}
+		ts := &metricspb.TimeSeries{
+			StartTimestamp: startTimestamp,
+			LabelValues:    []*metricspb.LabelValue{inEmptyValue, lv},
+			Points:         inPointsInt64_1,
 		}
-		server.resetStackdriverMetricDescriptors()
-		server.resetStackdriverTimeSeries()
+		tcs[0].inMetric[k].Timeseries = append(tcs[0].inMetric[k].Timeseries, ts)
+
+		j := i / 200
+		mt := outMetricTypeCalls
+		if k == 1 {
+			// TimeSeries Belongs to Latency
+			mt = outMetricTypeLatency
+		}
+		outTS := &monitoringpb.TimeSeries{
+			Metric: &googlemetricpb.Metric{
+				Type: mt,
+				Labels: map[string]string{
+					"empty_key":      "",
+					"operation_type": v,
+				},
+			},
+			Resource:   outGlobalResource,
+			MetricKind: googlemetricpb.MetricDescriptor_CUMULATIVE,
+			ValueType:  googlemetricpb.MetricDescriptor_INT64,
+			Points:     outPointsInt64_1,
+		}
+		tcs[0].outTSR[j].TimeSeries = append(tcs[0].outTSR[j].TimeSeries, outTS)
+	}
+	for _, tc := range tcs {
+		executeTestCase(t, tc, se, server)
+	}
+}
+
+func createConn(t *testing.T, addr string) *grpc.ClientConn {
+	conn, err := grpc.Dial(addr, grpc.WithInsecure())
+	if err != nil {
+		t.Fatalf("Failed to make a gRPC connection to the agent: %v", err)
+	}
+	return conn
+}
+
+func createExporter(t *testing.T, conn *grpc.ClientConn, opts sd.Options) *sd.Exporter {
+	opts.MonitoringClientOptions = []option.ClientOption{option.WithGRPCConn(conn)}
+	se, err := sd.NewExporter(opts)
+	if err != nil {
+		t.Fatalf("Failed to create the statsExporter: %v", err)
+	}
+	return se
+}
+
+func executeTestCase(t *testing.T, tc *testCases, se *sd.Exporter, server *fakeMetricsServer) {
+	dropped, err := se.PushMetricsProto(context.Background(), nil, nil, tc.inMetric)
+	if dropped != 0 || err != nil {
+		t.Fatalf("Name: %s, Error pushing metrics, dropped:%d err:%v", tc.name, dropped, err)
+	}
+
+	var gotTimeSeries []*monitoringpb.CreateTimeSeriesRequest
+	server.forEachStackdriverTimeSeries(func(sdt *monitoringpb.CreateTimeSeriesRequest) {
+		gotTimeSeries = append(gotTimeSeries, sdt)
+	})
+
+	if diff, idx := requireTimeSeriesRequestEqual(t, gotTimeSeries, tc.outTSR); diff != "" {
+		t.Errorf("Name[%s], TimeSeries[%d], Error: %s", tc.name, idx, diff)
+	}
+
+	var gotCreateMDRequest []*monitoringpb.CreateMetricDescriptorRequest
+	server.forEachStackdriverMetricDescriptor(func(sdm *monitoringpb.CreateMetricDescriptorRequest) {
+		gotCreateMDRequest = append(gotCreateMDRequest, sdm)
+	})
+
+	if diff, idx := requireMetricDescriptorRequestEqual(t, gotCreateMDRequest, tc.outMDR); diff != "" {
+		t.Errorf("Name[%s], MetricDescriptor[%d], Error: %s", tc.name, idx, diff)
+	}
+	server.resetStackdriverMetricDescriptors()
+	server.resetStackdriverTimeSeries()
+}
+
+func createMetricPbDescriptor(name, desc, unit string, mt metricspb.MetricDescriptor_Type, lblKeys ...*metricspb.LabelKey) *metricspb.MetricDescriptor {
+	return &metricspb.MetricDescriptor{
+		Name:        name,
+		Description: desc,
+		LabelKeys:   lblKeys,
+		Unit:        unit,
+		Type:        mt,
+	}
+}
+
+func createGoogleMetricPbDescriptor(name, metricType, desc, disp, unit string, mk googlemetricpb.MetricDescriptor_MetricKind, vt googlemetricpb.MetricDescriptor_ValueType, lblKeys ...string) *googlemetricpb.MetricDescriptor {
+	lbls := make([]*labelpb.LabelDescriptor, 0)
+	for _, k := range lblKeys {
+		lbl := &labelpb.LabelDescriptor{
+			Key:       k,
+			ValueType: labelpb.LabelDescriptor_STRING,
+		}
+		lbls = append(lbls, lbl)
+	}
+
+	return &googlemetricpb.MetricDescriptor{
+		Name:        name,
+		Type:        metricType,
+		Labels:      lbls,
+		MetricKind:  mk,
+		ValueType:   vt,
+		Unit:        unit,
+		Description: desc,
+		DisplayName: disp,
 	}
 }
 
@@ -283,7 +515,7 @@ type fakeMetricsServer struct {
 }
 
 func createFakeServer(t *testing.T) (*fakeMetricsServer, string, func()) {
-	ln, err := net.Listen("tcp", ":0")
+	ln, err := net.Listen("tcp", "localhost:0")
 	if err != nil {
 		t.Fatalf("Failed to bind to an available address: %v", err)
 	}
