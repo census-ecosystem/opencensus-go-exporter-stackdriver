f228aac17dbe648f967cf04a3a0edb369ef08d31
songy23 <songya@google.com>
2019-09-11 17:52:09 -0700

    Revert "Use endpoint instead of connection in equivalence_test (#207)"
    This reverts commit b2a517bda184b183a206dbf0e3b8104cf1802ebe.

--- equivalence_test.go
+++ equivalence_test.go
@@ -132,10 +132,17 @@ func TestEquivalenceStatsVsMetricsUploads(t *testing.T) {
 	server, addr, doneFn := createFakeServer(t)
 	defer doneFn()
 
+	// Now create a gRPC connection to the agent.
+	conn, err := grpc.Dial(addr, grpc.WithInsecure())
+	if err != nil {
+		t.Fatalf("Failed to make a gRPC connection to the agent: %v", err)
+	}
+	defer conn.Close()
+
 	// Finally create the OpenCensus stats exporter
 	exporterOptions := Options{
 		ProjectID:               "equivalence",
-		MonitoringClientOptions: []option.ClientOption{option.WithEndpoint(addr)},
+		MonitoringClientOptions: []option.ClientOption{option.WithGRPCConn(conn)},
 
 		// Setting this time delay threshold to a very large value
 		// so that batching is performed deterministically and flushing is
