0a6eaaea182aa55a878bcce0d568e34acea75c9e
Nick Travers <n.e.travers@gmail.com>
2020-01-02 12:35:41 -0800

    Update documentation
    Signed-off-by: Nick Travers <n.e.travers@gmail.com>

--- stackdriver.go
+++ stackdriver.go
@@ -259,7 +259,7 @@ type Options struct {
 	ReportingInterval time.Duration
 
 	// NumberOfWorkers sets the number of go rountines that send requests
-	// to Stackdriver Monitoring. The minimum number of workers is 1.
+	// to Stackdriver Monitoring and Trace. The minimum number of workers is 1.
 	NumberOfWorkers int
 
 	// ResourceByDescriptor may be provided to supply monitored resource dynamically
